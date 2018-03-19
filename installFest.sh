17dfb5006be3a57ef68fcc23e861a6f9d0d968bc
hsidar <marcus.estremera@gmail.com>
2018-03-18 23:57:07 -0400

    Update installFest.sh

--- installFest.sh
+++ installFest.sh
@@ -1,34 +1,37 @@
+cp -v ./commandLine/.bash_profile ~
+cp -v ./commandLine/.zshrc ~
+cp -v git/.gitconfig ~
+cp -v git/.gitignore_global ~
+cp -v git/git-completion.bash ~
+
 if ! type brew > /dev/null 2>&1; then
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
 	echo "Homebrew already installed"
 fi
 
-if ! type zsh > /dev/null 2>&1; then
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-else
-	echo "Zsh already installed"
-fi
-
 if ! type nvm > /dev/null 2>&1; then
 	brew install nvm
+	nvm install node
 else
 	echo "NVM already installed"
 fi
 
-programs=(sublime-text google-chrome brackets brave firefox karabiner-elements iterm2)
+programs=(sublime-text visual-studio-code google-chrome brave firefox karabiner-elements iterm2 mas)
 
 for program in ${programs[@]}
 do : 
    brew cask install $program
 done
 
-cp -v ./commandLine/.bash_profile ~
-cp -v ./commandLine/.zshrc ~
-cp -v git/.gitconfig ~
-cp -v git/.gitignore_global ~
-cp -v git/git-completion.bash ~
+if mas account; then
+	# Better Snap Tool
+	mas install 417375580
+	# Flycut
+	mas install 442160987
+else
+	echo "Not signed in to App Store"
+fi
+
 source ~/.bash_profile
 source ~/.zshrc
-
-defaults write com.apple.desktopservices DSDontWriteNetworkStores true
